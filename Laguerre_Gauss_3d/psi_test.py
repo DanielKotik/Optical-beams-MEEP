76c144dad9692b415a6c076bc46128a9c71a1e30
Daniel Kotik <daniel.kotik@freenet.de>
2018-01-29 00:25:14 +0100

    add suffix 'cartesian' to functions, correct integrand fucntion

--- Laguerre_Gauss_3d/psi_test.py
+++ Laguerre_Gauss_3d/psi_test.py
@@ -1,5 +1,7 @@
 """
-Check Meep's integration routine.
+Purposes: - check Meep's integration routine
+          - check implementation of Laguerre-Gauss beam fields
+          - check transition from cartesian to spherical coordinates
 """
 
 import numpy as np
@@ -23,40 +25,48 @@ def complex_dblquad(func, a, b, gfun, hfun, **kwargs):
     return (real_integral()[0] + 1j * imag_integral()[0], real_integral()[1:], imag_integral()[1:])
 
 
-## test paramters
+## test paramters (free space propagation, i.e. n1=n2=n_ref=1.0)
+kw_0  = 8.0
 freq  = 4.0
-k_vac = 2 * np.pi * freq
-w_0   = 8.0 / (2 * np.pi * 4)
 
-def f_Gauss(W_y, k_y, k_z): 
+## derived values
+k_vac = 2.0 * np.pi * freq
+w_0   = kw_0 / k_vac
+
+def f_Gauss_cartesian(W_y, k_y, k_z): 
     """
     """
     return sp.exp(-(W_y ** 2.0) * (k_y ** 2.0 + k_z ** 2.0) / 4.0)
 
-def integrand(x, y, z, k, k_y, k_z):
+def integrand_cartesian(x, y, z, k, k_y, k_z):
     """
     """
     ## first variant (taking the real part of the suqare root)
-    return f_Gauss(w_0, k_y, k_z) * sp.exp(1.0j * (x * (sp.sqrt(k**2 - k_y**2 - k_z**2) + y * k_y + z * k_z).real))
+    #return f_Gauss_cartesian(w_0, k_y, k_z) * sp.exp(1.0j * (x * (sp.sqrt(k**2 - k_y**2 - k_z**2).real) + 
+    #                                                         y * k_y + 
+    #                                                         z * k_z))
     
     ## second variant (leave square root as is, but perform second integration with non-constant bounds)
-    #return f_Gauss(w_0, k_y, k_z) * sp.exp(1.0j * (x * (sp.sqrt(k**2 - k_y**2 - k_z**2) + y * k_y + z * k_z)))
+    return f_Gauss_cartesian(w_0, k_y, k_z) * sp.exp(1.0j * (x * sp.sqrt(k**2 - k_y**2 - k_z**2) + 
+                                                             y * k_y + 
+                                                             z * k_z))
 
-def psi(x, y, z, k):
+def psi_cartesian(x, y, z, k):
     """
     """
-    integrand_ = lambda k_y, k_z: integrand(x, y, z, k, k_y, k_z)
+    integrand_ = lambda k_y, k_z: integrand_cartesian(x, y, z, k, k_y, k_z)
     
     ## constant integration bounds (appropriate for first variant)
-    return complex_dblquad(integrand_, -k, k, lambda x: -k, lambda x: k)[0]
+    #return complex_dblquad(integrand_, -k, k, lambda x: -k, lambda x: k)[0]
     
     ## non-constant integration bounds (appropriate for second variant)
-    #return complex_dblquad(integrand_, -k, k, lambda x: -np.sqrt(k**2 - x**2), lambda x: np.sqrt(k**2 - x**2))[0]
+    return complex_dblquad(integrand_, -k, k, lambda x: -np.sqrt(k**2 - x**2), lambda x: np.sqrt(k**2 - x**2))[0]
     
     
 x_shift = -2.0
-print "integrand: ", integrand(x_shift, 0.3, 0, k_vac, 4, 0)
-print "psi:       ", psi(x_shift, 0, 0, k_vac)
+print "Gauss spectrum (cartesian): ", f_Gauss_cartesian(w_0, 1.0, 5.2)
+print "integrand (cartesian):      ", integrand_cartesian(x_shift, 0.3, 0.5, k_vac, 4, 0)
+print "psi (cartesian):            ", psi_cartesian(x_shift, 0.3, 0.1, k_vac)
 
 K_y = np.linspace(-k_vac, k_vac, 200)
 
