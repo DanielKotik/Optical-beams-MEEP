f7cff1fd0a580e9539b698d91fc4d956df5dbaf8
Daniel Kotik <daniel.kotik@freenet.de>
2018-03-07 16:55:19 +0100

    Support for visualising cut-planes completed

--- Laguerre_Gauss_3d/plot_2d_matplotlib.py
+++ Laguerre_Gauss_3d/plot_2d_matplotlib.py
@@ -46,57 +46,94 @@ data = data[cutoff:-cutoff, cutoff:-cutoff, cutoff:-cutoff] / data.max()
 new_shape = np.shape(data)
 print "cutted shape:    ", new_shape
 
-center_z = int(np.rint((data.shape[2] - 1)/2))
-data_poi = data[:,:,center_z]                   # slice within the plane of incidence
-
+## calculate center of 3d array data in pixel coordinates
+center = (int((new_shape[0] - 1)/2), int((new_shape[1] - 1)/2), int(np.rint((data.shape[2] - 1)/2)))
 
 #------------------------------------------------------------------------------------------------------------------
-# calculating central k-vector components
+# calculating propagation directions of the secondary beams according to geometric optics
 #------------------------------------------------------------------------------------------------------------------
 eta_rad = np.arcsin((1.0/n) * np.sin(np.deg2rad(chi_deg)))   # angle of refraction in radians
 
 ## properties of the k-vectors
 vec_length = 150
 
-## (x,y)-components of the k-vectors (in pixel coordinates)
+## (x, y)-components of the central k-vectors (in pixel coordinates)
 center_x_y = (int((new_shape[0] - 1)/2), int((new_shape[1] - 1)/2))
-print center_x_y
-inc = (-vec_length, 0, 0)
-ref = (center_x_y[0] + int(np.rint(vec_length * np.sin(np.deg2rad(chi_deg - inc_deg)))),  
-       center_x_y[1] + int(np.rint(vec_length * np.cos(np.deg2rad(chi_deg - inc_deg)))))
-tra = (center_x_y[0] + int(np.rint(vec_length * np.sin(eta_rad + np.deg2rad(inc_deg)))), 
-       center_x_y[1] - int(np.rint(vec_length * np.cos(eta_rad + np.deg2rad(inc_deg)))))
-print ref
 
-#-------------------------------------------
-# define the position of the line 
-x0, y0 = center_x_y      # these are in pixel coordinates
-x1, y1 = tra
-length = int(np.hypot(x1-x0, y1-y0))
-x, y = np.linspace(x0, x1, length, dtype=np.int), np.linspace(y0, y1, length, dtype=np.int)
+vec_inc = (center[0] - vec_length, center[1])
+vec_ref = (center[0] + int(np.rint(vec_length * np.sin(np.deg2rad(chi_deg - inc_deg)))),
+           center[1] + int(np.rint(vec_length * np.cos(np.deg2rad(chi_deg - inc_deg)))))
+vec_tra = (center[0] + int(np.rint(vec_length * np.sin(eta_rad + np.deg2rad(inc_deg)))),
+           center[1] - int(np.rint(vec_length * np.cos(eta_rad + np.deg2rad(inc_deg)))))
 
-trans_slice_inc = data[x, y, :]
-print np.shape(trans_slice_inc)
+components = [vec_inc, vec_ref, vec_tra]
 
 
+#------------------------------------------------------------------------------------------------------------------
+# obtaining cut-plane data position
+#------------------------------------------------------------------------------------------------------------------
+delta_deg = 20           # opening angle
+
+## degree to radians conversion
+delta_rad = np.deg2rad(delta_deg)
+chi_rad   = np.deg2rad(chi_deg)
+inc_rad   = np.deg2rad(inc_deg)
+
+## calculate margins of the cut-plane for the respective beams in pixel coordinates
+cut_inc = (center[0] - vec_length, center[1] - int(np.rint(vec_length * np.tan(delta_rad))),
+           center[0] - vec_length, center[1] + int(np.rint(vec_length * np.tan(delta_rad))))
+cut_ref = (center[0] + int(np.rint((vec_length / np.cos(delta_rad)) * np.sin(chi_rad - delta_rad - inc_rad))),
+           center[1] + int(np.rint((vec_length / np.cos(delta_rad)) * np.cos(chi_rad - delta_rad - inc_rad))),
+           center[0] + int(np.rint((vec_length / np.cos(delta_rad)) * np.sin(chi_rad + delta_rad - inc_rad))),
+           center[1] + int(np.rint((vec_length / np.cos(delta_rad)) * np.cos(chi_rad + delta_rad - inc_rad))))
+cut_tra = (center[0] + int(np.rint((vec_length / np.cos(delta_rad)) * np.sin(eta_rad - delta_rad + inc_rad))),
+           center[1] - int(np.rint((vec_length / np.cos(delta_rad)) * np.cos(eta_rad - delta_rad + inc_rad))),
+           center[0] + int(np.rint((vec_length / np.cos(delta_rad)) * np.sin(eta_rad + delta_rad + inc_rad))),
+           center[1] - int(np.rint((vec_length / np.cos(delta_rad)) * np.cos(eta_rad + delta_rad + inc_rad))))
+
+x0, y0, x1, y1 = cut_tra
+width  = int(np.hypot(x1 - x0, y1 - y0))            # width of the cut-plane (determined by vec_length and delta_deg)
+x, y   = np.linspace(x0, x1, width, dtype=np.int), np.linspace(y0, y1, width, dtype=np.int)
+
+data_cut = data[x, y, :]  # TODO: Slice outside boundary possible? Fill with NaN?
 
 #------------------------------------------------------------------------------------------------------------------
 # visualising 
 #------------------------------------------------------------------------------------------------------------------
-fig, (ax1, ax2) = plt.subplots(1, 2)
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10,5))
 
 ## visualise intensity dirstribution within the plane of incidence
+data_poi = data[:,:,center[2]]                   # slice within the plane of incidence
 ax1.imshow(np.transpose(data_poi), origin="lower", cmap=plt.cm.hot, interpolation='None')
+
+## visualise k-vectors wihtin the plane of incidence
+for i in [0,1,2]:
+    ax1.plot([center[0], components[i][0]],
+             [center[1], components[i][1]], '--', color="white")
+
+## visualise cut line
 ax1.plot([x0, x1], [y0, y1], 'ro-')
+
+## subfigure properties
+ax1.set_title("plane of incidence")
 ax1.set_xlabel('x')                                 # labels are according to Meep
 ax1.set_ylabel('y')
 
 
-## visualise transverse intensity distribution with respect to the axes of the central wave vectors
-ax2.imshow(np.transpose(trans_slice_inc), origin="lower", cmap=plt.cm.hot, interpolation='None')
-ax2.set_xlabel('x')                                 # labels are according to Meep
+## visualise transverse intensity distribution with respect to the axis of the central wave vector
+ax2.imshow(np.transpose(data_cut), origin="lower", cmap=plt.cm.gist_stern_r, interpolation='None')
+
+## visualise geometric center point
+ax2.axhline(center[2], color='w', lw=0.5)
+ax2.axvline(int(np.rint((data_cut.shape[0] - 1)/2)), color='w', lw=0.5)
+
+## subfigure properties
+ax2.set_title("cut-plane")
+ax2.set_xlabel('x')                                 # labels are according to Bliokh&Aiello's beam coordinate system
 ax2.set_ylabel('z')
-#plt.colorbar()
+
+
+plt.tight_layout()
 plt.show()
 
 ## free memory
