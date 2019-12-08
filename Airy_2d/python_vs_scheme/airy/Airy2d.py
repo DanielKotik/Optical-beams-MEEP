4abaf4388d787c79a8c4e08ebc405332bff2af42
Daniel Kotik <daniel.kotik@freenet.de>
2019-12-09 00:31:14 +0100

    Fix formatting issues

--- Airy_2d/python_vs_scheme/airy/Airy2d.py
+++ Airy_2d/python_vs_scheme/airy/Airy2d.py
@@ -14,20 +14,21 @@ from datetime import datetime
 
 
 def Critical(n1, n2):
-    """Calculates the critical angle in degrees."""
-    raise NotImplementedError
-    
-def Brewster(n1, n2):
-    """Calculates the Brewster angle in degrees."""
+    """Calculate critical angle in degrees."""
     raise NotImplementedError
 
-    
+
+def Brewster(n1, n2):
+    """Calculate Brewster angle in degrees."""
+    raise NotImplementedError
+
+
 print("\nstart time:", datetime.now())
 
-#------------------------------------------------------------------------------
-# physical parameters characterizing light source and interface characteristics 
+# -----------------------------------------------------------------------------
+# physical parameters characterizing light source and interface characteristics
 # (must be adjusted - either here or via command line)
-#------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 s_pol = True
 ref_medium = 0
 
@@ -46,9 +47,9 @@ chi_deg = 45
 #chi_deg = 0.95*Brewster(n1, n2)
 
 
-#------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # specific Meep paramters (may need to be adjusted - either here or via command line)
-#------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 sx = 10
 sy = 10
 pml_thickness = 0.25
@@ -57,9 +58,9 @@ runtime = 90
 pixel = 6
 source_shift = 0
 
-#------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # derived Meep parameters (do not change)
-#------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 k_vac = 2 * math.pi * freq
 k1 = n1 * k_vac
 n_ref = 1.0
@@ -67,46 +68,46 @@ rw = kr_w / (n_ref * k_vac)
 w_0 = kw_0 / (n_ref * k_vac)
 shift = source_shift + rw
 
-#------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # placement of the dielectric interface within the computational cell
-#------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 cell = mp.Vector3(sx, sy, 0)  # geometry-lattice
 geometry = [mp.Block(mp.Vector3(mp.inf, 1, mp.inf),
                      center=mp.Vector3(),
                      material=mp.Medium(epsilon=12))]
 
 
-#------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # add absorbing boundary conditions and discretize structure
-#------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 pml_layers = [mp.PML(pml_thickness)]
-resolution = pixel * n1  * freq
+resolution = pixel * n1 * freq
 Courant = None
 
-#------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # beam profile distribution (field amplitude) at the waist of the beam
-#------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 def Gauss(r, W_y=w_0):
     """Gauss ..."""
     return NotImplementedError
 
 
 
-#------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # spectrum amplitude distribution
-#------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
-#------------------------------------------------------------------------------
-# plane wave decomposition 
+# -----------------------------------------------------------------------------
+# plane wave decomposition
 # (purpose: calculate field amplitude at light source position if not coinciding with beam waist)
-#------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
-#------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # display values of physical variables
-#------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 print("\n")
 print("Specified variables and derived values:")
-print("chi:  ", chi_deg, " [degree]") 
+print("chi:  ", chi_deg, " [degree]")
 print("incl.:", 90 - chi_deg, " [degree]")
 print("kw_0: ", kw_0)
 print("kr_w: ", kr_w)
@@ -114,9 +115,9 @@ print("k_vac:", k_vac)
 print("polarisation:", "s" if s_pol else "p")
 print("\n")
 
-#------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # specify current source, output functions and run simulation
-#------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 sources = [mp.Source(src=mp.ContinuousSource(frequency=0.15),
                      component=mp.Ez,
                      center=mp.Vector3(-7, 0))]
