533f70572d8fd5cfae525caedbf087943701cf94
Daniel Kotik <daniel.kotik@freenet.de>
2019-12-08 17:23:15 +0100

    Finalize simulation

--- Airy_2d/python_vs_scheme/airy/Airy2d.py
+++ Airy_2d/python_vs_scheme/airy/Airy2d.py
@@ -7,9 +7,9 @@ author:  Daniel Kotik
 version: X.X.X
 date:    30.11.2019
 """
-import meep as mp
 import math
-
+import meep as mp
+import numpy as np
 from datetime import datetime
 
 
@@ -70,7 +70,6 @@ shift = source_shift + rw
 #------------------------------------------------------------------------------
 # placement of the dielectric interface within the computational cell
 #------------------------------------------------------------------------------
-
 cell = mp.Vector3(sx, sy, 0)  # geometry-lattice
 geometry = [mp.Block(mp.Vector3(mp.inf, 1, mp.inf),
                      center=mp.Vector3(),
@@ -118,23 +117,9 @@ print("\n")
 #------------------------------------------------------------------------------
 # specify current source, output functions and run simulation
 #------------------------------------------------------------------------------
-
-
-print("\nend time:", datetime.now())
-"""
-cell = mp.Vector3(16, 8, 0)
-
-geometry = [mp.Block(mp.Vector3(mp.inf, 1, mp.inf),
-                     center=mp.Vector3(),
-                     material=mp.Medium(epsilon=12))]
-
-sources = [mp.Source(mp.ContinuousSource(frequency=0.15),
+sources = [mp.Source(src=mp.ContinuousSource(frequency=0.15),
                      component=mp.Ez,
                      center=mp.Vector3(-7, 0))]
-
-pml_layers = [mp.PML(1.0)]
-
-resolution = 10
 
 sim = mp.Simulation(cell_size=cell,
                     boundary_layers=pml_layers,
@@ -142,5 +127,11 @@ sim = mp.Simulation(cell_size=cell,
                     sources=sources,
                     resolution=resolution)
 
-sim.run(until=200)
-"""
+sim.run(until=runtime)
+
+eps_data = sim.get_array(center=mp.Vector3(), size=cell, component=mp.Dielectric)
+ez_data = sim.get_array(center=mp.Vector3(), size=cell, component=mp.Ez)
+np.save("eps_data", eps_data)
+np.save("ez_data", ez_data)
+
+print("\nend time:", datetime.now())
