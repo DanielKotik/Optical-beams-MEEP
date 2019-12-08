9a8fae5e478fd92d494d9a6c27643a2c2a44031f
Daniel Kotik <daniel.kotik@freenet.de>
2019-12-08 15:40:54 +0100

    Update Airy2d.py

--- Airy_2d/python_vs_scheme/airy/Airy2d.py
+++ Airy_2d/python_vs_scheme/airy/Airy2d.py
@@ -13,12 +13,12 @@ import math
 from datetime import datetime
 
 
-def ciritial_deg(n1, n2):
+def Critical(n1, n2):
     """Calculates the critical angle in degrees."""
     raise NotImplementedError
     
-def brewster_deg(n1, n2):
-    """calculates the Brewster angle in degrees."""
+def Brewster(n1, n2):
+    """Calculates the Brewster angle in degrees."""
     raise NotImplementedError
 
     
@@ -39,28 +39,57 @@ kr_w = 0
 M = 0
 W = 4
 
-
+chi_deg = 45
+#chi_deg = 0.95*Critical(n1, n2)
+#chi_deg = 0.95*Brewster(n1, n2)
 
 
 #------------------------------------------------------------------------------
 # specific Meep paramters (may need to be adjusted - either here or via command line)
 #------------------------------------------------------------------------------
+sx = 10
+sy = 10
+pml_thickness = 0.25
+freq = 5
+runtime = 90
+pixel = 6
+source_shift = 0
 
 #------------------------------------------------------------------------------
 # derived Meep parameters (do not change)
 #------------------------------------------------------------------------------
+k_vac = 2 * math.pi * freq
+k1 = n1 * k_vac
+n_ref = 1.0
+rw = kr_w / (n_ref * k_vac)
+w_0 = kw_0 / (n_ref * k_vac)
+shift = source_shift + rw
 
 #------------------------------------------------------------------------------
 # placement of the dielectric interface within the computational cell
 #------------------------------------------------------------------------------
 
+cell = mp.Vector3(sx, sy, 0)  # geometry-lattice
+geometry = [mp.Block(mp.Vector3(mp.inf, 1, mp.inf),
+                     center=mp.Vector3(),
+                     material=mp.Medium(epsilon=12))]
+
+
 #------------------------------------------------------------------------------
 # add absorbing boundary conditions and discretize structure
 #------------------------------------------------------------------------------
+pml_layers = [mp.PML(pml_thickness)]
+resolution = pixel * n1  * freq
+Courant = None
 
 #------------------------------------------------------------------------------
 # beam profile distribution (field amplitude) at the waist of the beam
 #------------------------------------------------------------------------------
+def Gauss(r, W_y=w_0):
+    """Gauss ..."""
+    return NotImplementedError
+
+
 
 #------------------------------------------------------------------------------
 # spectrum amplitude distribution
