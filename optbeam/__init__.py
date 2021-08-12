8d2dfc333d1907c57477512b968fbb5f8680f0bf
Daniel Kotik <kotik@physics.org>
2021-08-12 10:39:41 +0200

    Import only necessary functions and classes:
    Avoid namespace cluttering by importing with prefixed underscore.

--- optbeam/__init__.py
+++ optbeam/__init__.py
@@ -1,12 +1,18 @@
-import math
+
+from math import (degrees as _degrees,
+                  asin as _asin,
+                  atan as _atan)
+
+from ._2d.beams import Gauss2d, IncAiry2d
+from ._3d.beams import LaguerreGauss3d
 
 
 def critical(n1, n2):
     """Calculate critical angle in degrees."""
     assert n1 > n2, "\nWarning: Critical angle is not defined, since n1 <= n2!"
-    return math.degrees(math.asin(n2/n1))
+    return _degrees(_asin(n2/n1))
 
 
 def brewster(n1, n2):
     """Calculate Brewster angle in degrees."""
-    return math.degrees(math.atan(n2/n1))
+    return _degrees(_atan(n2/n1))
