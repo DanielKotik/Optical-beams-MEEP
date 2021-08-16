27c374dcb1af5794aa2823c0242c468a156bfb81
Daniel Kotik <kotik@physics.org>
2021-08-16 15:21:35 +0200

    Bump version: 2.0.3 → 2.1.0

--- optbeam/__init__.py
+++ optbeam/__init__.py
@@ -1,5 +1,5 @@
 
-__version__ = "2.0.3"
+__version__ = "2.1.0"
 
 from math import (degrees as _degrees,
                   asin as _asin,
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ extras = {
 
 setup(
     name="optbeam",
-    version="2.0.3",
+    version="2.1.0",
     description=("Simulation of reflection and refraction of polarized "
                  "opticial beams at plane and curved dielectric interfaces"),
     long_description=readme,
