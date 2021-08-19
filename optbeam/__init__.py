f475f798106a701ab455a2d4efd5d6f1e3ad9991
Daniel Kotik <kotik@physics.org>
2021-08-19 15:39:10 +0200

    Fix docs in `__init__.py`

--- optbeam/__init__.py
+++ optbeam/__init__.py
@@ -1,3 +1,15 @@
+"""optbeam: exact and fast calculation of optical beam profiles.
+
+Copyright (c) 2021 Daniel Kotik, Jörg B. Götte.
+All rights reserved.
+
+optbeam is a Cython-based package for fast calculation of exact field
+configurations of various optical beams in two and three dimensions.
+Intended for use together with `PyMeep`_.
+
+.. _PyMeep: https://meep.readthedocs.io/en/latest/
+
+"""
 
 __version__ = "2.1.2"
 
@@ -10,11 +22,39 @@ from ._3d.beams import LaguerreGauss3d
 
 
 def critical(n1, n2):
-    """Calculate critical angle in degrees."""
+    """Calculate critical angle.
+
+    Parameters
+    ----------
+    n1 : float
+        Index of refraction of the incident medium.
+    n2 : float
+        Index of refraction of the refractive medium.
+
+    Returns
+    -------
+    float
+        Angle in degrees.
+
+    """
     assert n1 > n2, "\nWarning: Critical angle is not defined, since n1 <= n2!"
     return _degrees(_asin(n2/n1))
 
 
 def brewster(n1, n2):
-    """Calculate Brewster angle in degrees."""
+    """Calculate Brewster angle.
+
+    Parameters
+    ----------
+    n1 : float
+        Index of refraction of the incident medium.
+    n2 : float
+        Index of refraction of the refractive medium.
+
+    Returns
+    -------
+    float
+        Angle in degrees.
+
+    """
     return _degrees(_atan(n2/n1))
