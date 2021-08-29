9a26746511db1ac7f17e511b5eaf05a7a35b795e
Daniel Kotik <kotik@physics.org>
2021-08-29 15:37:40 +0200

    Move `Beam3dSpherical` and `Beam3dCartesian` to `helpers.py`

--- optbeam/_3d/helpers.py
+++ optbeam/_3d/helpers.py
@@ -1,4 +1,5 @@
 
+import math
 import sys
 
 try:
@@ -13,6 +14,12 @@ if cython_imported:
     else:
         print("\nPlease consider compiling `%s.py` via Cython: "
               "`$ cythonize -3 -i %s.py`\n" % (__name__, __name__))
+
+if not cython_imported or not cython.compiled:
+    from math import (sin as _sin,
+                      cos as _cos)
+    from cmath import (exp as _cexp,
+                       sqrt as _csqrt)
 
 from scipy.integrate import dblquad
 from types import MappingProxyType
@@ -96,3 +103,88 @@ class Beam3d:
     def _integrand(self, x, y):
         """Integrand function over two coordinates x and y."""
         raise NotImplementedError
+
+
+class Beam3dSpherical(Beam3d):
+    """Reference implementaion of a 3d beam in spherical coordinates."""
+
+    def profile(self, r):
+        """Beam profile function, psi."""
+        self._ry = r.y
+        self._rz = r.z
+
+        if not self.called:
+            print("Calculating inital field configuration. "
+                  "This will take some time...")
+            self.called = True
+
+        try:
+            (result,
+             real_tol,
+             imag_tol) = _complex_dblquad(self if cython_imported and cython.compiled
+                                          else self._integrand,
+                                          0, 2*math.pi, 0, math.pi/2)
+        except Exception as e:
+            print(type(e).__name__ + ":", e)
+            sys.exit()
+
+        return self._k**2 * result
+
+    def spectrum(self, sin_theta, theta, phi):
+        """Spectrum amplitude function, f."""
+        raise NotImplementedError
+
+    def _phase(self, sin_theta, cos_theta, phi, x, y, z):
+        """Phase function."""
+        sin_phi = _sin(phi)
+        cos_phi = _cos(phi)
+
+        return self._k*(sin_theta*(y*sin_phi - z*cos_phi) + cos_theta*x)
+
+    def _integrand(self, theta, phi):
+        """Integrand function."""
+        sin_theta = _sin(theta)
+        cos_theta = _cos(theta)
+
+        return sin_theta * cos_theta * self.spectrum(sin_theta, theta, phi) * \
+            _cexp(1j*self._phase(sin_theta, cos_theta,
+                                 phi, self.x, self._ry, self._rz))
+
+
+class Beam3dCartesian(Beam3d):
+    """Reference implementaion of a 3d beam in Cartesian coordinates."""
+
+    def profile(self, r):
+        """Beam profile function, psi."""
+        self._ry = r.y
+        self._rz = r.z
+
+        if not self.called:
+            print("Calculating inital field configuration. "
+                  "This will take some time...")
+            self.called = True
+
+        try:
+            (result,
+             real_tol,
+             imag_tol) = _complex_dblquad(self if cython_imported and cython.compiled
+                                          else self._integrand,
+                                          -self._k, self._k, -self._k, self._k)
+        except Exception as e:
+            print(type(e).__name__ + ":", e)
+            sys.exit()
+
+        return result
+
+    def spectrum(self, k_y, k_z):
+        """Spectrum amplitude function, f."""
+        raise NotImplementedError
+
+    def _phase(self, k_y, k_z, x, y, z):
+        """Phase function."""
+        return x*_csqrt(self._k**2 - k_y**2 - k_z**2).real + y*k_y + z*k_z
+
+    def _integrand(self, k_y, k_z):
+        """Integrand function."""
+        return self.spectrum(k_y, k_z) * \
+            _cexp(1j*self._phase(k_y, k_z, self.x, self._ry, self._rz))
